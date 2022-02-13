56df883efe93fc4081b0f1738a7cb6fb0e5db1d9
LuisMirandaAcebedo <luis.miranda.acebedo@gmail.com>
2022-02-13 17:45:47 +0100

    Added BMP280 sensor and screen printing values

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
-
 from machine import Pin, SoftI2C
+import network
+from time import sleep_ms, sleep
+import bme280_float as bme280   #https://github.com/robert-hh/BME280
 import ssd1306
-from time import sleep_ms
 
-# ESP32 Pin assignment 
-#i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
-i2c = SoftI2C(scl=Pin(15), sda=Pin(4))
+led_onboard = Pin(2, Pin.OUT) #built-in blue LED
+led_onboard.value(1)
 
 pin_reset = Pin(16, Pin.OUT)
 pin_reset.value(0)
 sleep_ms(10)
 pin_reset.value(1)
 
-# ESP8266 Pin assignment
-#i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
+i2c = SoftI2C(scl=Pin(15), sda=Pin(4), freq=400000, timeout=255)
+print (i2c.scan()) #I2C test
 
 oled_width = 128
 oled_height = 64
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 
-oled.text('Hello, World 1!', 0, 0)
-oled.text('Hello, World 2!', 0, 10)
-oled.text('Hello, World 3!', 0, 20)
-        
-oled.show()
+bme = bme280.BME280(i2c=i2c)
+
+#Wifi test
+sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
+print (sta_if.scan())
+
+while True:
+    #print(bme.values)
+    t,p,h=bme.values #strings returned, for raw values use read_compensated_data
+    str1='Temp: '+t
+    str2='Press: '+p
+    str3='Hum: '+h
+    oled.fill(0)
+    oled.text('A Industriosa',12,50)
+    oled.text(str1,0,0)
+    oled.text(str2,0,10)
+    oled.text(str3,0,20)
+    oled.show()
+    led_onboard.value(not led_onboard.value())
+    sleep(1)
